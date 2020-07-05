a1efc922bbe1721e0ea9f11929a78243c36716d1
ikipyegon <ikipyegon@safaricom.co.ke>
2020-07-05 15:53:57 +0300

    refator(): change default mongo connection string

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -3,7 +3,7 @@ const appConfig = {
 };
 
 const dbConfig = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/notes-app-local'
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/fashion-cloud-api'
 };
 
 const logConfig = {
